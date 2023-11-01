cfd49e3cce5601d9444c28124def5b4d2b34c0a4
Nagateja Valiveti <teja91.valiveti@gmail.com>
2023-11-01 20:37:46 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import "@/app/ui/global.css";
 import { poppins } from "./ui/fonts";
+import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <NextTopLoader height={4} />
+        {children}
+      </body>
     </html>
   )
 }
